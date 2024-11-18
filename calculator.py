a9e081e55939c061e4b4e1b7f93225f9977b2716
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2024-11-18 11:58:55 +0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print(num1, "/", num2, "=", divide(num1,num2))
+  elif option == 5:
+    print("Thanks for playing with my calculator!")
+    break
+  else:
+    print("invalid input")
